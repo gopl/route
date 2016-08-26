be065da2a6508f87cfc1b7ecea4d09fa33178198
custa <custa@users.noreply.github.com>
2016-08-26 22:32:21 +0800

    Create mux.go

---
+++ mux/mux.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name"`
+	Email string `json:"email" xml:"email" form:"email"`
+}
+
+type Response struct {
+	ReturnCode string `json:"returnCode"`
+	Result     string `json:"result"`
+}
+
+func pathParameters(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	user := params["user"]
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "user="+user)
+}
+
+func queryParameters(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	user := params["user"] // 返回的是切片
+	io.WriteString(w, fmt.Sprintf("user=%v, params=%v", user, params))
+}
+
+func handlingRequest(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(io.LimitReader(r.Body, 80))
+	r.Body.Close()
+	var u User
+	if err := json.Unmarshal(body, &u); err != nil {
+		data, _ := json.Marshal(Response{"Failed", err.Error()})
+		w.Write([]byte(data))
+		return
+	}
+	fmt.Println(u)
+	data, _ := json.Marshal(Response{"OK", "Handle Request"})
+	w.Write([]byte(data))
+}
+
+// 认证
+func auth(username, password string) bool {
+	if username == "joe" && password == "secret" {
+		return true
+	}
+	return false
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.Methods("GET").
+		Path("/parameters/path/{user:user.*}"). // 支持正则表达式URL
+		Handler(http.HandlerFunc(pathParameters))
+	router.Methods("GET").
+		Path("/parameters/query").
+		Handler(http.HandlerFunc(queryParameters))
+
+	router.Methods("PUT").
+		Path("/handling/request").
+		Handler(http.HandlerFunc(handlingRequest))
+
+	log.Fatal(http.ListenAndServeTLS(":12345", "config/server-cert.pem",
+		"config/server-key.pem", router))
+}
